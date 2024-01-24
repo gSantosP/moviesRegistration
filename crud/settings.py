584f357da0b4c4c4dbdd0dd3aaf744e60ae689b1
Gabriel <gabriel7132@protonmail.com>
2024-01-24 10:29:37 -0300

    Removing Vercel files because it doesnt work with SQLite.

--- crud/settings.py
+++ crud/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example',
     'movies'
 ]
 
@@ -67,10 +66,10 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
